65af46b1b22d2f425a7b92a57da65adfce3314af
Facundo Arimany <arimanyfacundo@gmail.com>
2023-02-28 15:59:15 -0300

    DrumKit Full Updated

--- JsDrumKit.js
+++ JsDrumKit.js
@@ -6,11 +6,11 @@ window.addEventListener("keyup", (event) => {
 
 function DrumKit(event) {
   const keyCodesValidos = [65, 83, 68, 70, 71, 72, 74, 75, 76];
-  //console.log(event);
   if (!keyCodesValidos.includes(event.keyCode)) {
     return;
   }
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  audio.currentTime = 0;
   audio.play();
   const div = document.querySelector(`div[data-key="${event.keyCode}"]`);
   div.classList.add("playing");
